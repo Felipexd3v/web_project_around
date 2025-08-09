0efa0ba5db56dcde267cb4ac5ca8c0e1d4b110e7
FelipeCosta <felipealbuquerque116@gmail.com>
2025-08-09 11:13:50 -0300

    stop point

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 let editProfile = document.querySelector(".profile__button-edit");
+let addCard = document.querySelector(".profile__button-add");
 let popup = document.querySelector(".popup");
+let popupCard = document.querySelector(".popup-card");
 let buttonClose = document.querySelector(".popup__button-close");
+let buttonCloseCard = popupCard.querySelector(".popup__button-close");
 let nameProfile = document.querySelector(".profile__name");
 let experienceProfile = document.querySelector(".profile__experience");
 let inputName = document.querySelector("#name");
@@ -8,15 +11,21 @@ let inputExperience = document.querySelector("#experience");
 let formProfile = document.querySelector(".popup__edit-form");
 
 editProfile.addEventListener("click", openModal);
+addCard.addEventListener("click", openModalCard);
 
 function openModal() {
   popup.classList.add("popup_opened");
 }
 
-buttonClose.addEventListener("click", closeMOdal);
+function openModalCard() {
+  popupCard.classList.add("popup_opened");
+}
 
-function closeMOdal() {
-  popup.classList.remove("popup_opened");
+buttonClose.addEventListener("click", () => closeMOdal(popup));
+buttonCloseCard.addEventListener("click", () => closeMOdal(popupCard));
+
+function closeMOdal(modal) {
+  modal.classList.remove("popup_opened");
 }
 
 formProfile.addEventListener("submit", handleProfileFormSubmit);
